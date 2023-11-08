a4ebb4a58ce918edb01aacc24fe0fb0f72740bef
Lou Escobar <luisbermeo18@yahoo.com>
2023-11-07 19:43:34 -0500

    new branch

--- object.js
+++ object.js
@@ -16,17 +16,6 @@
 // const marginElement = document.getElementsByClassName (left-1)
 // marginElement.style.
 
-
-
-
-
-
-
-
-
-
-
-
 // key:value pairs, every key:value is separatd by "comma"
 const seattle = {
   name: "Seattle",
@@ -64,8 +53,8 @@ const seattleListEl = document.getElementById("seattle-list");
 let total = 0;
 
 for (let index = 0; index <= 13; index++) {
-  console.log('seattle cookies', seattle.cookies[index]);
-  console.log(typeof(seattle.cookies[index]));
+  console.log("seattle cookies", seattle.cookies[index]);
+  console.log(typeof seattle.cookies[index]);
   let li = document.createElement("li"); // <li></li>
 
   total += seattle.cookies[index];
@@ -87,16 +76,9 @@ for (let index = 0; index <= 13; index++) {
   li.innerText = formatedTime + ": " + seattle.cookies[index] + " cookies"; // <li>234</li>
   seattleListEl.appendChild(li);
 }
-let renderTotal = document.createElement('li');
+let renderTotal = document.createElement("li");
 renderTotal.textContent = `Total: ${total} Cookies`;
 seattleListEl.appendChild(renderTotal);
-
-
-
-
-
-
-
 
 const tokyo = {
   name: "Tokyo",
@@ -122,15 +104,13 @@ for (let hour = 6; hour <= 19; hour++) {
   tokyo.cookies.push(noOfCookies);
 }
 
-
-
 // DOM Manipulation
 const tokyoListEl = document.getElementById("tokyo-list");
 let tokyoTotal = 0;
 
 for (let index = 0; index <= 13; index++) {
-  console.log('tokyo cookies', tokyo.cookies[index]);
-  console.log(typeof(tokyo.cookies[index]));
+  console.log("tokyo cookies", tokyo.cookies[index]);
+  console.log(typeof tokyo.cookies[index]);
   let li = document.createElement("li"); // <li></li>
 
   tokyoTotal += tokyo.cookies[index];
@@ -152,32 +132,17 @@ for (let index = 0; index <= 13; index++) {
   li.innerText = formatedTime + ": " + tokyo.cookies[index] + " cookies"; // <li>234</li>
   tokyoListEl.appendChild(li);
 }
-let renderTokyoTotal = document.createElement('li');
+let renderTokyoTotal = document.createElement("li");
 renderTokyoTotal.textContent = `Total: ${tokyoTotal} Cookies`;
 tokyoListEl.appendChild(renderTokyoTotal);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 const dubai = {
-  name: 'Dubai',
+  name: "Dubai",
   minHourlyCustomers: 11,
   maxHourlyCustomers: 38,
   averageSale: 3.7,
-  cookies: []
-
-
-}
+  cookies: [],
+};
 
 for (let hour = 6; hour <= 19; hour++) {
   // console.log(`Hour ${hour}:00`);
@@ -194,15 +159,13 @@ for (let hour = 6; hour <= 19; hour++) {
   dubai.cookies.push(noOfCookies);
 }
 
-
-
 // DOM Manipulation
 const dubaiListEl = document.getElementById("dubai-list");
 let dubaiTotal = 0;
 
 for (let index = 0; index <= 13; index++) {
-  console.log('dubai cookies', dubai.cookies[index]);
-  console.log(typeof(dubai.cookies[index]));
+  console.log("dubai cookies", dubai.cookies[index]);
+  console.log(typeof dubai.cookies[index]);
   let li = document.createElement("li"); // <li></li>
 
   dubaiTotal += dubai.cookies[index];
@@ -224,41 +187,16 @@ for (let index = 0; index <= 13; index++) {
   li.innerText = formatedTime + ": " + dubai.cookies[index] + " cookies"; // <li>234</li>
   dubaiListEl.appendChild(li);
 }
-let renderDubaiTotal = document.createElement('li');
+let renderDubaiTotal = document.createElement("li");
 renderDubaiTotal.textContent = `Total: ${dubaiTotal} Cookies`;
 dubaiListEl.appendChild(renderDubaiTotal);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const paris = {
   name: "Paris",
   minHourlyCustomers: 20,
   maxHourlyCustomers: 38,
   averageSale: 2.3,
-  cookies: []
+  cookies: [],
 };
 for (let hour = 6; hour <= 19; hour++) {
   // console.log(`Hour ${hour}:00`);
@@ -275,15 +213,13 @@ for (let hour = 6; hour <= 19; hour++) {
   paris.cookies.push(noOfCookies);
 }
 
-
-
 // DOM Manipulation
 const parisListEl = document.getElementById("paris-list");
 let parisTotal = 0;
 
 for (let index = 0; index <= 13; index++) {
-  console.log('paris cookies', paris.cookies[index]);
-  console.log(typeof(paris.cookies[index]));
+  console.log("paris cookies", paris.cookies[index]);
+  console.log(typeof paris.cookies[index]);
   let li = document.createElement("li"); // <li></li>
 
   parisTotal += paris.cookies[index];
@@ -305,24 +241,16 @@ for (let index = 0; index <= 13; index++) {
   li.innerText = formatedTime + ": " + paris.cookies[index] + " cookies"; // <li>234</li>
   parisListEl.appendChild(li);
 }
-let renderParisTotal = document.createElement('li');
+let renderParisTotal = document.createElement("li");
 renderParisTotal.textContent = `Total: ${parisTotal} Cookies`;
 parisListEl.appendChild(renderParisTotal);
-
-
-
-
-
-
-
-
 
 const lima = {
   name: "Lima",
   minHourlyCustomers: 2,
   maxHourlyCustomers: 16,
   averageSale: 4.6,
-  cookies: []
+  cookies: [],
 };
 for (let hour = 6; hour <= 19; hour++) {
   // console.log(`Hour ${hour}:00`);
@@ -339,15 +267,13 @@ for (let hour = 6; hour <= 19; hour++) {
   lima.cookies.push(noOfCookies);
 }
 
-
-
 // DOM Manipulation
 const limaListEl = document.getElementById("lima-list");
 let limaTotal = 0;
 
 for (let index = 0; index <= 13; index++) {
-  console.log('lima cookies', lima.cookies[index]);
-  console.log(typeof(lima.cookies[index]));
+  console.log("lima cookies", lima.cookies[index]);
+  console.log(typeof lima.cookies[index]);
   let li = document.createElement("li"); // <li></li>
 
   limaTotal += lima.cookies[index];
@@ -369,8 +295,6 @@ for (let index = 0; index <= 13; index++) {
   li.innerText = formatedTime + ": " + lima.cookies[index] + " cookies"; // <li>234</li>
   limaListEl.appendChild(li);
 }
-let renderLimaTotal = document.createElement('li');
+let renderLimaTotal = document.createElement("li");
 renderLimaTotal.textContent = `Total: ${limaTotal} Cookies`;
 limaListEl.appendChild(renderLimaTotal);
-
-
